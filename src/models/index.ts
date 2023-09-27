d9e6a0453bec862f3a37825f2809592e00fa3754
Nilson Santiago <nilson.santiago19@gmail.com>
2023-09-26 21:13:42 -0300

    relacionamento entre modelos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,10 @@
 import { Category } from "./Category"
+import { Course } from "./Courses"
+
+Category.hasMany(Course)
+Course.belongsTo(Category)
 
 export {
-    Category
+    Category,
+    Course
 }
