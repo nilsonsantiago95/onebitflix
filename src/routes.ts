25251b0bcd212de13f3b5079216853a0308afd79
Nilson Santiago <nilson.santiago19@gmail.com>
2023-10-19 20:17:32 -0300

    Registro de usuários

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,11 @@ import express from "express";
 import { categorioriesController } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
+import { authController } from "./controllers/authCOntroller";
 
 const router = express.Router()
+
+router.post('/auth/register', authController.register)
 
 router.get('/categories', categorioriesController.index)
 router.get('/categories/:id', categorioriesController.show)
